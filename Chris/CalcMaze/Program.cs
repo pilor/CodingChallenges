47817c2af7d799c064efaee545ece309c4647626
Chris Eggert <eggertc@gmail.com>
2016-12-06 10:21:12 -0800

    Add empty sln

---
+++ Chris/CalcMaze/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("...");
+            Console.ReadKey();
+        }
+    }
+}
