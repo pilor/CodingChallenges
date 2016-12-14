337ed747f2053264ff409f292d6b2713ad8b9180
Luke Schoen <lukedude@gmail.com>
2016-12-14 13:15:44 -0800

    lazy man's hardcoded path to test case

--- Week 1/Luke/CalcMaze/Program.cs
+++ Week 1/Luke/CalcMaze/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CalcMaze
+﻿using System;
+
+namespace CalcMaze
 {
     using System.IO;
 
@@ -7,13 +9,21 @@
         private static void Main(string[] args)
         {
             var prog = new Program();
-            prog.Run(@"D:\Git\CodingChallenges\Week 1\Tests\Input1.txt");
+            string path = @"D:\Git\CodingChallenges\Week 1\Tests\Input1.txt";
+            if (!File.Exists(path))
+            {
+                path = @"C:\Git\CodingChallenges\Week 1\Tests\Input1.txt";
+            }
+            prog.Run(path);
         }
 
         private void Run(string inputPath)
         {
             string allText = File.ReadAllText(inputPath);
             var puzzle = InputParser.Parse(allText);
+            Console.WriteLine(puzzle.Solve());
+            Console.WriteLine("press enter to exit");
+            Console.ReadLine();
         }
     }
 }
