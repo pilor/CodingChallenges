21e81f13712587ff702a47d7248573506087203e
Luke Schoen <lukedude@gmail.com>
2016-12-14 15:02:39 -0800

    Initial commit with solution and test cases - no code

---
+++ Week 1/Karen/Week1_Maze/Week1_Maze/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1_Maze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get the first number
+            string mazeResult           ;
+
+            // get the first line of numbers
+
+            // figure out how many numbers the line has
+            // save such number for maze size. 
+            int mazeSize;
+
+
+        }
+    }
+}
